717ec18b718d6188a4bb21cf063c9a12d1bb0553
dwauto7 <dwautomate7@gmail.com>
2025-11-13 18:32:07 +0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.https://urqqmhsuvjngraweruba.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycXFtaHN1dmpuZ3Jhd2VydWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMTMzMTIsImV4cCI6MjA3ODU4OTMxMn0.9apzq-U2htbingBtC3DD_p_3pLhjpCNSmP9oF16bbrE;
 
 let supabase: SupabaseClient | null = null;
 
